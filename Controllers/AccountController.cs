67277dfa49b57c06a013b6f00f070cd7c0ee56e9
Lovisa Landberg <lovisa@landberg.org>
2026-03-15 08:39:28 +0100

    Skapad accountcontroller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using databasteknik.models;
+
+namespace databasteknik.Controllers;
+
+public class AccountController : Controller
+{
+    // Hårdkodad demo-användare
+    private static User demoUser = new User
+    {
+        UserId = 1,
+        UserName = "test",
+        Password = "1234"
+    };
+
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+		// Kontrollera användarnamn och lösenord mot demo-användaren
+        if(username == demoUser.UserName && password == demoUser.Password)
+        {
+            // Spara användarnamnet i session
+            HttpContext.Session.SetString("CurrentUser", demoUser.UserName);
+
+			// Vid lyckad inloggning, omdirigera till spellistor
+            return RedirectToAction("MyPlaylists", "Playlist");
+        }
+
+		// Om inloggningen misslyckas, visa ett felmeddelande
+        ViewBag.Error = "Fel användarnamn eller lösenord";
+        return View();
+    }
+
+    public IActionResult Logout()
+	{
+		// Rensar sessionen
+		HttpContext.Session.Clear();
+
+		// Återställer spellistorna
+		PlaylistController.ResetPlaylists();
+
+		// Omdirigera till inloggningssidan
+		return RedirectToAction("Login");
+	}
+}
